6d2d0847c8510fe1b684265928f4e57da2d1a3eb
HalfaPencil <77938269+HalfaPencil@users.noreply.github.com>
2026-05-27 20:09:31 -0400

    Initial Commit

---
+++ fitnessapp/fitnessappApp.swift
@@ -0,0 +1,17 @@
+//
+//  fitnessappApp.swift
+//  fitnessapp
+//
+//  Created by Tony Lin on 2026-05-27.
+//
+
+import SwiftUI
+
+@main
+struct fitnessappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
